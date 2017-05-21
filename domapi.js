974487929f2ff0e2236e036df001b43165d07647
madgeni <nick.madge@gmail.com>
2017-05-21 17:15:49 +0100

    updated format

--- domapi.js
+++ domapi.js
@@ -1,24 +1,21 @@
+'use strict'
+let handleControl = require('./domo-code/Control')
+let handleDiscovery = require('./domo-code/Discovery')
 
-var handleControl = require('./domo-code/Control');
-var handleDiscovery = require('./domo-code/Discovery');
-
-
-//This is the heart of the code - takes the request/response headers for Alexa
-var func =  function (event, context) {
-
-    switch (event.header.namespace) {
-
-        case 'Alexa.ConnectedHome.Discovery':
-            handleDiscovery(event, context);
-            break;
-        case 'Alexa.ConnectedHome.Control':
-        case 'Alexa.ConnectedHome.Query':
-            handleControl(event, context);
-            break;
-        default:
-            console.log('Err', 'No supported namespace: ' + event.header.namespace);
-            context.fail('Something went wrong');
-            break;
-    }
-};
-exports.handler = func;
+// This is the heart of the code - takes the request/response headers for Alexa
+let func = function (event, context) {
+  switch (event.header.namespace) {
+    case 'Alexa.ConnectedHome.Discovery':
+      handleDiscovery(event, context)
+      break
+    case 'Alexa.ConnectedHome.Control':
+    case 'Alexa.ConnectedHome.Query':
+      handleControl(event, context)
+      break
+    default:
+      console.log('Err', 'No supported namespace: ' + event.header.namespace)
+      context.fail('Something went wrong')
+      break
+  }
+}
+exports.handler = func
